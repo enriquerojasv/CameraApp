fbcc385fbce7be370a32460ab60a7c3853718811
enriquerojasv <erojasv@cifpfbmoll.eu>
2020-11-30 20:14:15 +0100

    Initial commit

---
+++ app/src/main/java/com/androidclase/cameraapp/MainActivity.java
@@ -0,0 +1,73 @@
+package com.androidclase.cameraapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int CAMERA_PERM_CODE = 0;
+    public static final int CAMERA_REQUEST_CODE = 2;
+    ImageView photoView;
+    Button btnCamera, btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCamera = (Button) findViewById(R.id.btnCamera);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        photoView = (ImageView) findViewById(R.id.photoView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Camera btn Clicked", Toast.LENGTH_SHORT).show();
+                askCameraPermissions();
+            }
+        });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Save btn Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+            openCamera();
+        }
+    }
+
+    private void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            photoView.setImageBitmap(image);
+        }
+    }
+}
